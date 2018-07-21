d174ae1558d91619aad86104cab0dde60cd0f678
AaroonKd <40718689+AaroonKd@users.noreply.github.com>
2018-07-21 23:00:55 +0200

    asd

---
+++ AndroidGames/Assets/Editor Default Resources/UMotionEditor/Scripts/Editor/GUICompatibilityUtility.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+
+namespace UMotionEditor
+{
+    public static class GUICompatibilityUtility
+    {
+        //********************************************************************************
+        // Public Properties
+        //********************************************************************************
+
+        public static event Action HierarchyChanged
+        {
+            add
+            {
+                #if UNITY_2018_1_OR_NEWER
+                EditorApplication.hierarchyChanged += value;
+                #else
+                hierarchyChangedInternal += value;
+                if (!hierarchyHelperEventSetup)
+                {
+                    EditorApplication.hierarchyWindowChanged += OnHierarchyChanged_HelperEvent;
+                    hierarchyHelperEventSetup = true;
+                }
+                #endif
+            }
+            remove
+            {
+                #if UNITY_2018_1_OR_NEWER
+                EditorApplication.hierarchyChanged -= value;
+                #else
+                hierarchyChangedInternal -= value;
+                #endif
+            }
+        }
+
+        //********************************************************************************
+        // Private Properties
+        //********************************************************************************
+
+        //----------------------
+        // Inspector
+        //----------------------
+
+        //----------------------
+        // Internal
+        //----------------------
+        #if !UNITY_2018_1_OR_NEWER
+        private static bool hierarchyHelperEventSetup = false;
+        private static event Action hierarchyChangedInternal;
+        #endif
+
+        //********************************************************************************
+        // Public Methods
+        //********************************************************************************
+
+        public static Color ColorField(GUIContent label, Color value, bool showEyedropper, bool showAlpha, bool hdr, params GUILayoutOption[] options)
+        {
+            #if UNITY_2018_1_OR_NEWER
+            return EditorGUILayout.ColorField(label, value, showEyedropper, showAlpha, hdr, options);
+            #else
+            return EditorGUILayout.ColorField(label, value, showEyedropper, showAlpha, hdr, null, options);
+            #endif
+        }
+
+        //********************************************************************************
+        // Private Methods
+        //********************************************************************************
+
+        #if !UNITY_2018_1_OR_NEWER
+        private static void OnHierarchyChanged_HelperEvent()
+        {
+            if (hierarchyChangedInternal != null)
+            {
+                hierarchyChangedInternal();
+            }
+        }
+        #endif
+    }
+}
